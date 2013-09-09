55ee698e0a720e4e19c3a10ff9d07c52ad739fba
Richard Goulter <richard.goulter@gmail.com>
2013-09-09 11:24:27 +0800

    An actual test. THIS SHOULD WORK.

--- basicsTests/basictests.cpp
+++ basicsTests/basictests.cpp
@@ -1,16 +1,27 @@
 #include "gtest/gtest.h"
+#include "li.h"
+#include "pointSet.h"
 
-TEST(BasicsTest, ASimpleTest) {
- 
-   float _Base = 4.0f;
-   float _Exponent = 2.0f;
- 
-   float _ExpectedValue = 8.0f;
- 
-   float _ActualValue = _Base * _Exponent;
- 
-   EXPECT_FLOAT_EQ(_ExpectedValue, _ActualValue);  // Almost equal
- 
+TEST(BasicsTest, PointSetInTriSimple) {
+	PointSet ps;
+	
+	int p1 = ps.addPoint(0, 0);   // 1
+	int p2 = ps.addPoint(0, 4);   // 2
+	int p3 = ps.addPoint(4, 0);   // 3
+	int p4 = ps.addPoint(4, 4);   // 4
+	int p5 = ps.addPoint(1, 1);   // 5
+
+	// In case the above goes out of range
+	ps.addPoint(1, 1);   // 6
+
+	// (1, 1) IS in tri 123
+	EXPECT_EQ(1, ps.inTri(p1, p2, p3, p5));
+	
+	// (1, 1) IS in tri 124
+	EXPECT_EQ(0, ps.inTri(p1, p2, p4, p5));
+	
+	// (1, 1) IS NOT in tri 234
+	EXPECT_EQ(-1, ps.inTri(p2, p3, p4, p5));
 }
 
 
