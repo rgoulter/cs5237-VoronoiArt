b6b6c7603369abf2012b1a8f588cac6a2e9cd935
Richard Goulter <richard.goulter@gmail.com>
2013-10-06 15:40:16 +0800

    Dumb mistake; slight syntax error.

--- include/basics/pointSet.h
+++ include/basics/pointSet.h
@@ -42,7 +42,7 @@ public:
 	int addPoint(LongInt x1,LongInt y1); 
 	// add an ith(according to the adding sequence)  point (x1,y1), return i.
 
-	void PointSet::deleteLastPoint();
+	void deleteLastPoint();
 
 	int inTri(int p1Idx, int p2Idx, int p3Idx, int pIdx);
 	// return 1 if the point p is inside the triangle p1p2p3, by reference of the indices, -1 if outside, 0 if degenerate
