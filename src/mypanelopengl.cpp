d833eb7063d8e01909c5710d07b39db52298fb7a
Richard Goulter <richard.goulter@gmail.com>
2013-11-22 17:38:55 +0800

    Save image now reads from the Front buffer, so it is current. :-)

--- src/mypanelopengl.cpp
+++ src/mypanelopengl.cpp
@@ -879,13 +879,13 @@ void MyPanelOpenGL::doSaveImage() {
 	unsigned char *data;
 	data = (unsigned char *) malloc(numComponents * copyWidth * copyHeight * sizeof(unsigned char));
 	
-
     glPixelStorei(GL_PACK_ALIGNMENT, 1); // align to the byte..
+	glReadBuffer(GL_FRONT);
 
 	// Read in "correct" row...
 	// (I think I have to do this since the texture coordinates' y-axis is flipped
 	//  compared to the rendered y-axis).
-
+	
 	for (int rowOffset = 0; rowOffset < copyHeight; rowOffset++) {
 		int copyRow = y + copyHeight - rowOffset - 1; // reverse this.
 		unsigned char * copyAddress = data + rowOffset * (copyWidth * numComponents);
