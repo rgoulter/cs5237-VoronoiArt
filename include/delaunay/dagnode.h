a647bffc342c9a43fe5669e412a0e9569d4c83a5
Richard Goulter <richard.goulter@gmail.com>
2016-07-12 21:27:51 +0800

    Move DAGNode to dagnode.h/cpp

---
+++ include/delaunay/dagnode.h
@@ -0,0 +1,30 @@
+#ifndef DAGNODEH
+#define DAGNODEH
+
+#include <vector>
+
+#include "pointset.h"
+#include "pointsetarray.h"
+#include "triangle.h"
+
+
+
+class DAGNode {
+public:
+	static std::vector<DAGNode*> leafNodesContainingPoint(DAGNode* root, const PointSetArray& pointSet, int pIdx);
+
+	/// 'contains edge' iff 'contains both points'.
+	/// Order of points not considered.
+	static std::vector<DAGNode*> leafNodesContainingEdge(DAGNode* root, const PointSetArray& pointSet, int pIdx1, int pIdx2);
+
+	static int findAdjacentTriangle(DAGNode* root, const PointSetArray& pointSet, int pIdx1, int pIdx2, int pIdx3);
+
+	DAGNode(const TriRecord& tri) : tri_(tri) {}
+
+	bool isLeaf() const { return children_.empty(); }
+
+	TriRecord tri_;
+	std::vector<DAGNode *> children_;
+};
+
+#endif
--- include/delaunay/directedgraph.h
+++ include/delaunay/directedgraph.h
@@ -6,29 +6,10 @@
 #include <iostream>
 #include <vector>
 
+#include "dagnode.h"
 #include "pointset.h"
 #include "pointsetarray.h"
 #include "triangle.h"
-
-
-
-class DAGNode {
-public:
-	static std::vector<DAGNode*> leafNodesContainingPoint(DAGNode* root, const PointSetArray& pointSet, int pIdx);
-
-	/// 'contains edge' iff 'contains both points'.
-	/// Order of points not considered.
-	static std::vector<DAGNode*> leafNodesContainingEdge(DAGNode* root, const PointSetArray& pointSet, int pIdx1, int pIdx2);
-
-	static int findAdjacentTriangle(DAGNode* root, const PointSetArray& pointSet, int pIdx1, int pIdx2, int pIdx3);
-
-	DAGNode(const TriRecord& tri) : tri_(tri) {}
-
-	bool isLeaf() const { return children_.empty(); }
-
-	TriRecord tri_;
-	std::vector<DAGNode *> children_;
-};
 
 
 
