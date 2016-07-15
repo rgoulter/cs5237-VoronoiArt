8ec48baefc1c2821d2730cdc08692c82575e4e16
Richard Goulter <richard.goulter@gmail.com>
2016-07-16 00:53:57 +0800

    Add test for polygons from edges

--- test/voronoi/polyconstructtests.cpp
+++ test/voronoi/polyconstructtests.cpp
@@ -1,19 +1,24 @@
 #include <cmath>
 
+#include <set>
 #include <vector>
 
 #include "gtest/gtest.h"
 
 #include "delaunay/pointsetarray.h"
+#include "delaunay/polygon.h" // for orientation
 #include "delaunay/directedgraph.h"
 
 #include "voronoi/polyconstruct.h"
 
+using std::set;
 using std::vector;
 using std::cout;
 using std::endl;
 
+using delaunay::MyPoint;
 using delaunay::PointSetArray;
+using delaunay::orientation;
 
 using namespace voronoi;
 
@@ -115,3 +120,69 @@ TEST(VoronoiPolyConstructTest, LinkEdgesTrivialCase) {
 // Could test polygonFromLinkedEdge as intermediate
 
 
+
+TEST(VoronoiPolyConstructTest, PolygonsFromEdgesTrivialCase) {
+	// VEdge isn't quite pair<Pt,Pt>; needs 'start'
+	VPoint *s = new VPoint(0, 0);
+
+	VPoint *a = new VPoint(10, 10);
+	VPoint *b = new VPoint(50, 50);
+	VPoint *c = new VPoint(90, 10);
+
+	VEdge *ab = new VEdge(s, a, b);
+	VEdge *bc = new VEdge(s, b, c);
+	VEdge *ca = new VEdge(s, c, a);
+
+	Edges edges;
+	edges.push_back(ab);
+	edges.push_back(bc);
+	edges.push_back(ca);
+
+	// Should return tri<abc> and tri<cba>
+	vector<PointSetArray> polygons = polygonsFromEdges(edges);
+	bool isCCW[2];
+
+	ASSERT_EQ(2, polygons.size());
+
+	// The following is *really* awkward,
+	// partly since atm we don't have good abstractions
+	// for dealing with polygons.
+
+	// Need to check that got CCW order.
+	for (int polyIdx = 0; polyIdx < polygons.size(); ++polyIdx) {
+		const PointSetArray& poly = polygons[polyIdx];
+		for (int ptIdx = 1; ptIdx <= poly.noPt(); ++ptIdx) {
+			const MyPoint& p1 = poly[ptIdx];
+
+			// Looking for the starting point, (10, 10)
+			if (abs(p1.x.doubleValue() - 10) < 1) {
+				// next 1=>2, 2=>3, 3=>1
+				const MyPoint& p2 = poly[(ptIdx) % 3 + 1];
+				// next 1=>3, 2=>1, 3=>2
+				const MyPoint& p3 = poly[(ptIdx + 1) % 3 + 1];
+
+				int or1 = orientation(p1, p2, p3);
+				int or2 = orientation(p2, p3, p1);
+				int or3 = orientation(p3, p1, p2);
+				EXPECT_TRUE(or1 == or2 && or2 == or3);
+
+				isCCW[polyIdx] = (or1 == 1);
+
+				break;
+			}
+		}
+	}
+
+	// One of them will be true
+	EXPECT_TRUE(isCCW[0] || isCCW[1]);
+	// Exactly one. (One is CCW, the other isn't).
+	EXPECT_TRUE((!isCCW[0] && isCCW[1]) ||
+	            (isCCW[0] && !isCCW[1]));
+
+	// Clean up...
+	delete ab;
+	delete bc;
+	delete a;
+	delete b;
+	delete c;
+}
